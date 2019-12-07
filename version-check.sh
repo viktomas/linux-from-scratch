ef1d2db5e725e4eca349183b087a7aa15adcf6e0
Tomas <vicek22@gmail.com>
2019-12-07 17:57:50 +0100

    Update from LFS9

--- version-check.sh
+++ version-check.sh
@@ -3,23 +3,38 @@
 
 export LC_ALL=C
 bash --version | head -n1 | cut -d" " -f2-4
-echo "/bin/sh -> `readlink -f /bin/sh`"
+MYSH=$(readlink -f /bin/sh)
+echo "/bin/sh -> $MYSH"
+echo $MYSH | grep -q bash || echo "ERROR: /bin/sh does not point to bash"
+unset MYSH
+
 echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
 bison --version | head -n1
-if [ -e /usr/bin/yacc ];
-  then echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`"; 
-  else echo "yacc not found"; fi
+
+if [ -h /usr/bin/yacc ]; then
+  echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
+elif [ -x /usr/bin/yacc ]; then
+  echo yacc is `/usr/bin/yacc --version | head -n1`
+else
+  echo "yacc not found" 
+fi
 
 bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-
 echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
 diff --version | head -n1
 find --version | head -n1
 gawk --version | head -n1
-if [ -e /usr/bin/awk ];
-  then echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`"; 
-  else echo "awk not found"; fi
+
+if [ -h /usr/bin/awk ]; then
+  echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
+elif [ -x /usr/bin/awk ]; then
+  echo awk is `/usr/bin/awk --version | head -n1`
+else 
+  echo "awk not found" 
+fi
 
 gcc --version | head -n1
+g++ --version | head -n1
 ldd --version | head -n1 | cut -d" " -f2-  # glibc version
 grep --version | head -n1
 gzip --version | head -n1
@@ -28,13 +43,14 @@ m4 --version | head -n1
 make --version | head -n1
 patch --version | head -n1
 echo Perl `perl -V:version`
+python3 --version
 sed --version | head -n1
 tar --version | head -n1
-echo "Texinfo: `makeinfo --version | head -n1`"
+makeinfo --version | head -n1  # texinfo version
 xz --version | head -n1
 
-echo 'main(){}' > dummy.c && gcc -o dummy dummy.c
-if [ -x dummy ] 
-  then echo "gcc compilation OK";
-  else echo "gcc compilation failed"; fi
+echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
+if [ -x dummy ]
+  then echo "g++ compilation OK";
+  else echo "g++ compilation failed"; fi
 rm -f dummy.c dummy
